23f528756efcfebe42ff742c845c74169901e4dd
woutdenolf <woutdenolf@users.sf.net>
2022-11-10 18:46:21 +0100

    support positional arguments

--- src/ewoksppf/ppfrunscript.py
+++ src/ewoksppf/ppfrunscript.py
@@ -4,20 +4,22 @@ from ewokscore.inittask import instantiate_task
 INFOKEY = "_noinput"
 
 
-def run(**inputs):
+def run(*args, **kwargs):
     """Main of actor execution.
 
     :param **kw: output hashes from previous tasks
     :returns dict: output hashes
     """
-    info = inputs.pop(INFOKEY)
+    info = kwargs.pop(INFOKEY)
     varinfo = info["varinfo"]
     execinfo = info["execinfo"]
+    if args:
+        kwargs.update(enumerate(args))
 
     task = instantiate_task(
         info["node_id"],
         info["node_attrs"],
-        inputs=inputs,
+        inputs=kwargs,
         varinfo=varinfo,
         execinfo=execinfo,
     )
