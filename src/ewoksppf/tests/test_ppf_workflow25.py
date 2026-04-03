c50c41e3a6485eb6847490ded3e0403647357c99
woutdenolf <woutdenolf@users.sf.net>
2026-04-03 19:05:53 +0200

    workflow25: stop triggering based on cache and not link-to-self

--- src/ewoksppf/tests/test_ppf_workflow25.py
+++ src/ewoksppf/tests/test_ppf_workflow25.py
@@ -1,5 +1,9 @@
+import json
 import time
+from pathlib import Path
+from typing import Dict
 
+import pytest
 from ewokscore.task import Task
 from ewoksutils.import_utils import qualname
 
@@ -28,82 +32,93 @@ class Timeout(Task, input_names=["timeout"], output_names=["timeout"]):
 class Decider(
     Task,
     input_names=["param1", "param2", "metric_threshold"],
-    optional_input_names=[
-        "metric1",
-        "metric2",
-        "metric3",
-        "metric4",
-        "timeout",
-        "disable",
-    ],
-    output_names=["good_enough", "reason", "set_disable"],
+    optional_input_names=["metric1", "metric2", "metric3", "metric4", "timeout"],
+    output_names=["good_enough", "reason"],
 ):
     def run(self):
-        print("\nDecider inputs:", self.get_input_values())
-        print("Metric threshold:", self.inputs.metric_threshold)
+        print("\nDecider executed with inputs:", self.get_input_values())
+
+        # Check that we have all required inputs
         if self.inputs.param1 != -1:
             raise ValueError(f"param1 is {self.inputs.param1} intead of -1")
 
         if self.inputs.param2 != -2:
             raise ValueError(f"param2 is {self.inputs.param2} intead of -2")
 
-        if self.inputs.disable:
-            print("IGNORE")
-            self.outputs.good_enough = None
-            self.outputs.reason = None
-            self.outputs.set_disable = False
-            return
-
+        # Check the state of all metrics
         good_metrics = []
-        nmetrics = 0
-        for name in ["metric1", "metric2", "metric3", "metric4"]:
+        bad_metrics = []
+        unknown_metrics = []
+        metric_names = ["metric1", "metric2", "metric3", "metric4"]
+        for name in metric_names:
             value = self.get_input_value(name, None)
             if value is None:
-                continue
-            nmetrics += 1
-            if value >= self.inputs.metric_threshold:
+                unknown_metrics.append(name)
+            elif value >= self.inputs.metric_threshold:
                 good_metrics.append(name)
-        print("Metrics that are good:", good_metrics)
+            else:
+                bad_metrics.append(name)
 
-        if any(good_metrics):
+        print("Metrics that are good:", good_metrics)
+        print("Metrics that are bad:", bad_metrics)
+        print("Metrics that are unknown:", unknown_metrics)
+
+        # Check timeout
+        has_timeout = bool(self.get_input_value("timeout"))
+        print("Timeout:", "yes" if has_timeout else "no")
+
+        # Trigger downstream reasons
+        trigger_good = len(good_metrics) == 1
+        trigger_bad_timeout = has_timeout
+        trigger_bad_none_good_enough = len(unknown_metrics) == 0
+
+        # Ensure good/bad is trigger only once
+        n = sum((bool(good_metrics), trigger_bad_timeout, trigger_bad_none_good_enough))
+        if n > 1:
+            trigger_good = False
+            trigger_bad_timeout = False
+            trigger_bad_none_good_enough = False
+
+        # Trigger good, bas or nothing
+        if trigger_good:
             print("TRIGGER GOOD")
             self.outputs.good_enough = True
             self.outputs.reason = ", ".join(good_metrics)
-            self.outputs.set_disable = True
-            return
-
-        if self.get_input_value("timeout"):
-            print("TRIGGER BAD")
+        elif trigger_bad_timeout:
+            print("TRIGGER BAD: TIMEOUT")
             self.outputs.good_enough = False
             self.outputs.reason = "timeout"
-            self.outputs.set_disable = True
-            return
-
-        if nmetrics == 4:
-            print("TRIGGER BAD")
+        elif trigger_bad_none_good_enough:
+            print("TRIGGER BAD: ALL METRIC BAD")
             self.outputs.good_enough = False
-            self.outputs.reason = "none"
-            self.outputs.set_disable = True
-            return
-
-        print("TRIGGER NOTHING")
-        self.outputs.good_enough = None
-        self.outputs.reason = None
-        self.outputs.set_disable = False
+            self.outputs.reason = "no metric good enough"
+        else:
+            print("TRIGGER NOTHING")
+            self.outputs.good_enough = None
+            self.outputs.reason = None
 
 
-class Good(Task, input_names=["reason"], output_names=["state", "reason"]):
+class RecordCalls(Task, input_names=["call_record_file"]):
+    def run(self):
+        calls = json.loads(self.inputs.call_record_file.read_text())
+        calls.append(self.get_output_values())
+        self.inputs.call_record_file.write_text(json.dumps(calls))
+
+
+class Good(RecordCalls, input_names=["reason"], output_names=["state", "reason"]):
     def run(self):
         print("\nGOOD:", self.inputs.reason)
         self.outputs.state = "GOOD"
         self.outputs.reason = self.inputs.reason
+        super().run()
 
 
-class Bad(Task, input_names=["reason"], output_names=["state", "reason"]):
+class Bad(RecordCalls, input_names=["reason"], output_names=["state", "reason"]):
     def run(self):
         print("\nBAD:", self.inputs.reason)
         self.outputs.state = "BAD"
         self.outputs.reason = self.inputs.reason
+        super().run()
 
 
 def workflow():
@@ -161,15 +176,6 @@ def workflow():
         },
         {
             "source": "decider",
-            "target": "decider",
-            "cache_if_optional": True,
-            "data_mapping": [
-                {"source_output": "set_disable", "target_input": "disable"}
-            ],
-            "conditions": [{"source_output": "set_disable", "value": True}],
-        },
-        {
-            "source": "decider",
             "target": "good",
             "data_mapping": [{"source_output": "reason", "target_input": "reason"}],
             "conditions": [{"source_output": "good_enough", "value": True}],
@@ -184,7 +190,7 @@ def workflow():
     return {"graph": {"id": "workflow"}, "nodes": nodes, "links": links}
 
 
-def create_inputs(metric_threshold: float):
+def create_inputs(call_record_file: Path, metric_threshold: float):
     tm = 0.1
     return [
         {"id": "required", "name": "compute_time", "value": 0.1 * tm},
@@ -198,10 +204,12 @@ def create_inputs(metric_threshold: float):
         {"id": "metric4", "name": "compute_time", "value": 5 * tm},
         {"id": "metric4", "name": "metric", "value": 40},
         {"id": "decider", "name": "metric_threshold", "value": metric_threshold},
+        {"id": "good", "name": "call_record_file", "value": call_record_file},
+        {"id": "bad", "name": "call_record_file", "value": call_record_file},
     ]
 
 
-def create_inputs_timeout_last(metric_threshold: float):
+def create_inputs_timeout_last(call_record_file: Path, metric_threshold: float):
     tm = 0.1
     return [
         {"id": "required", "name": "compute_time", "value": 0.1 * tm},
@@ -215,52 +223,84 @@ def create_inputs_timeout_last(metric_threshold: float):
         {"id": "metric4", "name": "metric", "value": 40},
         {"id": "timeout", "name": "timeout", "value": 5 * tm},
         {"id": "decider", "name": "metric_threshold", "value": metric_threshold},
+        {"id": "good", "name": "call_record_file", "value": call_record_file},
+        {"id": "bad", "name": "call_record_file", "value": call_record_file},
     ]
 
 
-def test_ppf_workflow25_metric1(ppf_log_config):
+@pytest.fixture
+def call_record_file(tmp_path: Path):
+    file = tmp_path / "calls.json"
+    file.write_text(json.dumps([]))
+    return file
+
+
+def test_ppf_workflow25_metric1(ppf_log_config, call_record_file: Path):
     """test 'cache_if_optional' links"""
     # Metrics that pass: metric1, metric2, metric3 and metric4
-    inputs = create_inputs(5)
+    inputs = create_inputs(call_record_file, 5)
     result = execute_graph(workflow(), inputs=inputs)
-    assert result == {"reason": "metric1", "state": "GOOD"}
+
+    expected = {"state": "GOOD", "reason": "metric1"}
+    calls = json.loads(call_record_file.read_text())
+    assert calls == [expected]
+
+    assert result == expected
 
 
-def test_ppf_workflow25_metric2(ppf_log_config):
+def test_ppf_workflow25_metric2(ppf_log_config, call_record_file: Path):
     """test 'cache_if_optional' links"""
     # Metrics that pass: metric2, metric3 and metric4
-    inputs = create_inputs(15)
+    inputs = create_inputs(call_record_file, 15)
     result = execute_graph(workflow(), inputs=inputs)
-    assert result == {"reason": "metric2", "state": "GOOD"}
+
+    expected = {"state": "GOOD", "reason": "metric2"}
+    _assert_result(call_record_file, result, expected)
 
 
-def test_ppf_workflow25_metric3(ppf_log_config):
+def test_ppf_workflow25_metric3(ppf_log_config, call_record_file: Path):
     """test 'cache_if_optional' links"""
     # Metrics that pass: metric3 and metric4
-    inputs = create_inputs(25)
+    inputs = create_inputs(call_record_file, 25)
     result = execute_graph(workflow(), inputs=inputs)
-    assert result == {"reason": "metric3", "state": "GOOD"}
+
+    expected = {"state": "GOOD", "reason": "metric3"}
+    _assert_result(call_record_file, result, expected)
 
 
-def test_ppf_workflow25_timeout1(ppf_log_config):
+def test_ppf_workflow25_timeout1(ppf_log_config, call_record_file: Path):
     """test 'cache_if_optional' links"""
     # Metrics that pass: metric4
-    inputs = create_inputs(35)
+    inputs = create_inputs(call_record_file, 35)
     result = execute_graph(workflow(), inputs=inputs)
-    assert result == {"reason": "timeout", "state": "BAD"}
+
+    expected = {"state": "BAD", "reason": "timeout"}
+    _assert_result(call_record_file, result, expected)
 
 
-def test_ppf_workflow25_timeout2(ppf_log_config):
+def test_ppf_workflow25_timeout2(ppf_log_config, call_record_file: Path):
     """test 'cache_if_optional' links"""
     # Metrics that pass: none
-    inputs = create_inputs(45)
+    inputs = create_inputs(call_record_file, 45)
     result = execute_graph(workflow(), inputs=inputs)
-    assert result == {"reason": "timeout", "state": "BAD"}
+
+    expected = {"state": "BAD", "reason": "timeout"}
+    _assert_result(call_record_file, result, expected)
 
 
-def test_ppf_workflow25_none(ppf_log_config):
+def test_ppf_workflow25_timeout3(ppf_log_config, call_record_file: Path):
     """test 'cache_if_optional' links"""
     # Metrics that pass: none
-    inputs = create_inputs_timeout_last(45)
+    inputs = create_inputs_timeout_last(call_record_file, 45)
     result = execute_graph(workflow(), inputs=inputs)
-    assert result == {"reason": "none", "state": "BAD"}
+
+    expected = {"state": "BAD", "reason": "no metric good enough"}
+    _assert_result(call_record_file, result, expected)
+
+
+def _assert_result(
+    call_record_file: Path, result: Dict[str, str], expected: Dict[str, str]
+) -> None:
+    calls = json.loads(call_record_file.read_text())
+    assert calls == [expected]
+    assert result == expected
